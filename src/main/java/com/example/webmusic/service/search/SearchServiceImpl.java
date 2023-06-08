3cf43b62f008e86289006144899587a1092ce483
lpt <373595331@qq.com>
2023-06-08 19:24:50 +0800

    修改了了前端模型

--- src/main/java/com/example/webmusic/service/search/SearchServiceImpl.java
+++ src/main/java/com/example/webmusic/service/search/SearchServiceImpl.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.webmusic.controller.search.In.InApiSearchByKeyword;
 import com.example.webmusic.controller.search.out.OutApiSearchSongs;
-import com.example.webmusic.frontend_model.SongFront;
 import com.example.webmusic.mapper.song.SongMapper;
 import com.example.webmusic.models.song.Song;
 import com.example.webmusic.service.album.AlbumService;
@@ -41,26 +40,5 @@ public class SearchServiceImpl implements SearchService{
         List<Song> songList = songPage.getRecords();
         long pageTotal = songPage.getTotal();
         //执行完毕
-        List<SongFront> songFronts = new ArrayList<>();
-        for (int i = 0; i < songList.size(); i++) {
-            SongFront sf = SongFront.builder()
-                    .songId(songList.get(i).getSong_id())
-                    .album(songList.get(i).getAlbum())
-                    .albumId(songList.get(i).getAlbum_id())
-                    .picUrl(songList.get(i).getPic_url())
-                    .pop(songList.get(i).getPop())
-                    .type(songList.get(i).getType())
-                    .artist(songList.get(i).getArtist())
-                    .duration(songList.get(i).getDuration())
-                    .lyricUrl(songList.get(i).getLyric_url())
-                    .publishTime(songList.get(i).getPublish_time())
-                    .artistId(songList.get(i).getArtist_id())
-                    .mark(songList.get(i).getMark())
-                    .name(songList.get(i).getName()).build();
-            songFronts.add(sf);
-        }
-        outApiSearchSongs.setCode(200);
-        outApiSearchSongs.setSongFronts(songFronts);
-        outApiSearchSongs.setPageTotal(pageTotal);
     }
 }
