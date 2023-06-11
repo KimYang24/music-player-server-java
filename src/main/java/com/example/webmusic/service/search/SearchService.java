3815147859c3cc28392cd3f1e4f1c0b863ed624f
lpt <373595331@qq.com>
2023-06-11 11:56:33 +0800

    debug

--- src/main/java/com/example/webmusic/service/search/SearchService.java
+++ src/main/java/com/example/webmusic/service/search/SearchService.java
@@ -1,14 +1,12 @@
 package com.example.webmusic.service.search;
 
-import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.webmusic.controller.search.In.InApiSearchByKeyword;
 import com.example.webmusic.controller.search.out.*;
-import com.example.webmusic.models.artist.Artist;
 
 public interface SearchService{
-    public void searchArtistsByKeyword(InApiSearchByKeyword inApiSearchByKeyword, OutApiSearchArtists outApiSearchArtists);
-    public void searchAlbumsByKeyword(InApiSearchByKeyword inApiSearchByKeyword, OutApiSearchAlbums outApiSearchAlbums);
-    public void searchSongsByKeyword(InApiSearchByKeyword inApiSearchByKeyword, OutApiSearchSongs outApiSearchSongs);
-    public void searchHot(OutApiSearchHot outApiSearchHot);
-    public void searchHotByKeyWord(String keyword, OutApiHotByKeyword outApiHotByKeyword);
+    void searchArtistsByKeyword(InApiSearchByKeyword inApiSearchByKeyword, OutApiSearchArtists outApiSearchArtists);
+    void searchAlbumsByKeyword(InApiSearchByKeyword inApiSearchByKeyword, OutApiSearchAlbums outApiSearchAlbums);
+    void searchSongsByKeyword(InApiSearchByKeyword inApiSearchByKeyword, OutApiSearchSongs outApiSearchSongs);
+    void searchHot(OutApiSearchHot outApiSearchHot);
+    void searchHotByKeyWord(String keyword, OutApiHotByKeyword outApiHotByKeyword);
 }
--- src/main/java/com/example/webmusic/service/search/SearchServiceImpl.java
+++ src/main/java/com/example/webmusic/service/search/SearchServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.webmusic.service.search;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
